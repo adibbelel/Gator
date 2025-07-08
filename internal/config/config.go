24d74ccc146f6e414cff9fb6ec5bd1eb28c605d9
adibbelel <adibbelel1@gmail.com>
2025-07-08 12:04:38 +0100

    feat: add config package

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+  "os"
+  "encoding/json"
+  "io/ioutil"
+  "fmt"
+)
+
+type Config struct {
+  DbURL string `json:"db_url"`
+  CurrentUserName string `json:"current_user_name"`
+}
+
+const configFileName = ".gatorconfig.json"
+
+func Read() (Config, error) {
+  data := os.UserHomeDir() + configFileName
+  jsonData, err := ioutil.ReadFile(data)
+  if err != nil {
+    return nil, fmt.Errorf("Error reading file")
+  }
+
+  var config Config
+  err := json.Unmarshal([]byte(jsonData), &config)
+  if err != nil {
+    return nil, fmt.Errorf("could not unmarshal jsonData")
+  }
+
+  return config, nil
+}
+
+func (c *Config) SetUser(username string) {
+  c.CurrentUserName := username
+  updatedData, err := json.Marshal(c)
+  if err != nil {
+    fmt.Errorf("Could not Marshal Data")
+  }
+
+  err = json.WriteFile((os.UserHomeDir() + configFileName), updatedData, 0644)
+  if err != nil {
+    fmt.Errorf("Could not update data in json file")
+    return 
+  }
+}
