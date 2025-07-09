a57b6a6a0d43645728a5de647ef8b6286a5ade4d
adibbelel <adibbelel1@gmail.com>
2025-07-09 16:44:30 +0100

    feat: add run and register commands(#4)

---
+++ commands.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "github.com/adibbelel/gator/internal/config"
+)
+
+type state struct {
+  cfg *Config 
+}
+
+type command struct {
+  name string
+  inputs []string
+}
+
+var commands struct {
+  registeredCommands map[string]func(*state, commmand) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+  if len(cmd.inputs) != 1 {
+    return fmt.Errorf("wrong usage")
+  }
+
+  name := cmd.inputs[0]
+
+  err := s.cfg.SetUser(name)
+  if err != nil {
+    return fmt.Errorf("Failed to set user") 
+  }
+  fmt.Println("User has been set to -", s.cfg.CurrentUserName)
+  return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+  function, ok := c.registeredCommands[cmd.name]
+  if !ok {
+    return fmt.Errorf("command not found")
+  }
+  return function(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+  c.registeredCommands[name] = f
+}
